e172e082be8378df616f12f0d48353728ed4d4de
yukokurose <adelkoko@mail.ru>
2025-02-04 23:27:59 +0700

    first commit

---
+++ Vue/js/main.js
@@ -0,0 +1,13 @@
+let product = "Socks";
+
+app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "../assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        inventory: 100,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral']
+    }
+})git init
