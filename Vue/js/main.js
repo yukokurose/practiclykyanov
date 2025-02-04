cdf1af4f6f8eec3facf4fe62c04b27a9fe062e20
yukokurose <adelkoko@mail.ru>
2025-02-05 00:44:28 +0700

    5-6 practich

--- Vue/js/main.js
+++ Vue/js/main.js
@@ -4,38 +4,44 @@ app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "../assets/vmSocks-blue-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         altText: "A pair of socks",
-        inStock: true,
-        inventory: 100,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "../assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "../assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
-
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
-
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         }
     },
-
-
-
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    }
 })
