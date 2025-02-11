8da3951f51852fba9a3724fdc9442780fe615ced
yukokurose <adelkoko@mail.ru>
2025-02-11 22:46:10 +0700

    zadanie2

--- Vue/js/main.js
+++ Vue/js/main.js
@@ -42,6 +42,7 @@ new Vue({
                     const completed = card.items.filter(i => i.completed).length
                     const total = card.item.length
                     const progress = completed / total
+
                     if (card.column === 1 && progress > 0.5){
                         if(this.secondColumnCards.length < 5){
                             card.column = 2
@@ -54,6 +55,42 @@ new Vue({
                     }
                 })
                 this.saveCard()
+            }
+        }
+    },
+    method: {
+        addItem(){
+            if(this.newCardItems.length < 5){
+                this.newCardItems.push('')
+            }
+        },
+        removeItem(){
+            if(this.new.CardItems.length > 3){
+                this.newCardItems.pop()
+            }
+        },
+        createCard(){
+            if(!this.newCardTitle.trim() || this.newCardItems.some(i => !i.trim())){
+                alert('PLease fill all fields')
+                return
+            }
+            this.cards.push({
+                id: Date.now(),
+                title: this.newCardTitle,
+                items: this.newCardItems.map(text => ({text, completed: false})),
+                column: 1,
+                completedDate: null
+            })
+            this.newCardTitle = ''
+            this.newCardItems = ['', '', '']
+        },
+        saveCards(){
+            localStorage.setItem('cards',JSON.stringify(this.cards))
+        },
+        loadCards(){
+            const savedCards = localStorage.getItem('cards')
+            if(savedCards) {
+                this.cards = JSON.parse(savedCards)
             }
         }
     }
